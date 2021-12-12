b44cadc53babbbd4c5ea735afba7083d18592eda
LAUBOONCHEONG <laubclcs-wm19@student.tarc.edu.my>
2021-12-12 16:26:02 +0800

    changes

---
+++ Game Assignment/Assets/Script/Sequences/DerrickTrigger.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+using UnityEngine.UI;
+
+public class DerrickTrigger : MonoBehaviour
+{
+    public float TheDistance;
+    public GameObject ThePlayer;
+    public GameObject TextBox;
+    public AudioSource Audio3;
+    public GameObject ActionDisplay;
+    public GameObject ActionText;
+    public GameObject ExtraCross;
+
+    void Update()
+    {
+        TheDistance = PlayerCasting.DistanceFromTarget;
+    }
+
+    void OnMouseOver()
+    {
+        if (TheDistance <= 2)
+        {
+            ExtraCross.SetActive(true);
+            ActionDisplay.SetActive(true);
+            ActionText.SetActive(true);
+        }
+        if (Input.GetButtonDown("Action"))
+        {
+            if (TheDistance <= 2)
+            {
+                ActionDisplay.SetActive(false);
+                ActionText.SetActive(false);
+                ThePlayer.GetComponent<FirstPersonController>().enabled = false;
+                StartCoroutine(ScenePlayer());
+            }
+        }
+    }
+    IEnumerator ScenePlayer() {
+        TextBox.GetComponent<Text>().text = "Look Like A Weapon On The Table";
+        Audio3.Play();
+        yield return new WaitForSeconds(1.5f);
+        TextBox.GetComponent<Text>().text = "";
+        ThePlayer.GetComponent<FirstPersonController>().enabled = true;
+    }
+}
+
